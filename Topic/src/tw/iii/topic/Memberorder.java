17e8ea8038f726f33df0b24fc01a3a315d525157
kk6665555 <49990115@stust.edu.tw>
2017-09-09 15:29:19 +0800

    0909

--- Topic/src/tw/iii/topic/Memberorder.java
+++ Topic/src/tw/iii/topic/Memberorder.java
@@ -12,10 +12,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Memberorder")
 public class Memberorder extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		
+		if(session.getAttribute("id")!=null) {
+			session.removeAttribute("id");
+		}
+		
 		String select = request.getParameter("select");
 		String name = request.getParameter("name");
 		String phone = request.getParameter("phone");
